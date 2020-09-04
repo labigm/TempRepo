8fabde6e0dbda14f4b43efa56e31cc0c6ce6e5be
BSAlberto <BSAlberto@gmail.com>
2020-09-04 09:53:00 -0400

    This is the first push to this repo

---
+++ DeleteMe/DeleteMe/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 0 -> 0000
+            // 1 -> 0001
+            // 2 -> 0010
+            // 3 -> 0011
+            // 4 -> 0100
+            int varA = 36; // Declaring the variable
+            varA = varA + 1;
+            varA *= 2;
+            varA -= 1;
+            varA++;
+            Console.WriteLine("My Age " + varA);
+        }
+    }
+}
